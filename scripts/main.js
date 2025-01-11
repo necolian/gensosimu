62e8bdf99ecf0d5daccf5d13ba7013474251aac6
necolian <25159102-necolian@users.noreply.gitlab.com>
2025-01-11 05:25:19 +0000

    Add main.js

---
+++ scripts/main.js
@@ -0,0 +1,14 @@
+//設定用変数 jsonで設定
+var config = {
+  type: Phaser.AUTO,
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create,
+    update: update
+  }
+};
+
+//インスタンス作成
+var game = new Phaser.Game(config);
