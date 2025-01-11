b78a305513f5136d3daefe47dd503cb20694d217
necolian <25159102-necolian@users.noreply.gitlab.com>
2025-01-11 08:12:01 +0000

    assets-preload.js を編集

--- scripts/assets-preload.js
+++ scripts/assets-preload.js
@@ -1,3 +1,10 @@
-export function preload() {
+function preload() {
+  alert("preload");
   this.load.image("background","./assets/background.png");
 }
+
+function pre_alert(){
+  alert("pre-alert");
+}
+
+export {preload,pre_alert}
