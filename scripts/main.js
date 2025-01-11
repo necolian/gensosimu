a84f356a72965885dbb4c5dac035869b9c13465d
necolian <25159102-necolian@users.noreply.gitlab.com>
2025-01-11 05:31:22 +0000

    main.js を編集

--- scripts/main.js
+++ scripts/main.js
@@ -1,8 +1,8 @@
 //設定用変数 jsonで設定
 var config = {
   type: Phaser.AUTO,
-  width: 800,
-  height: 600,
+  width: 800, //横サイズ
+  height: 600, //縦サイズ
   scene: {
     preload: preload,
     create: create,
@@ -13,14 +13,17 @@ var config = {
 //インスタンス作成
 var game = new Phaser.Game(config);
 
+//画像プリロード
 function preload() {
-
+  
 }
 
+//描画
 function create() {
-
+  
 }
 
+//更新
 function update() {
   
 }
