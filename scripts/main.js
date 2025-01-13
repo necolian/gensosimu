04df173ec1f62f221ea03821e6e947dcbc106cb8
necolian <25159102-necolian@users.noreply.gitlab.com>
2025-01-13 14:55:24 +0000

    main.js を編集

--- scripts/main.js
+++ scripts/main.js
@@ -2,13 +2,13 @@ let auto_center;
 let zoom;
 
 if (window.innerWidth < window.innerHeight) {
-  zoom = window.innerWidth / 600
+  zoom = window.innerHeight / 600
   auto_center = Phaser.Scale.Center.CENTER_VERTICALLY;
 }else if (window.innerWidth === window.innerHeight) {
   zoom = window.innerHeight / 600;
   auto_center = Phaser.Scale.Center.CENTER_BOTH;
 }else{
-  zoom = window.innerHeight / 800;
+  zoom = window.innerWidth / 800;
   auto_center = Phaser.Scale.Center.CENTER_HORIZONTALLY;
 }
 
